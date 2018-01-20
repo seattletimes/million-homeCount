cb78ddf0be6614a9960b1774243af9a2e92815a2
emilymeng <eeng@seattletimes.com>
2018-01-19 16:15:52 -0800

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,85 @@
+//load our custom elements
+require("component-leaflet-map");
+require("component-responsive-frame");
+
+//get access to Leaflet and the map
+var element = document.querySelector("leaflet-map");
+var L = element.leaflet;
+var map = element.map;
+
+//ICH code for popup template if needed----------
+var ich = require("icanhaz");
+var templateFile = require("./_popup.html");
+ich.addTemplate("popup", templateFile);
+
+var data = require("./million-home-count.geo.json");
+
+data.features.forEach(function(f) {
+	["2014", "2017"].forEach(function(prop) {
+		f.properties[prop] = (f.properties[prop] * 100).toFixed(1);
+	});
+});
+var mapElement = document.querySelector("leaflet-map");
+
+if (mapElement) {
+  var L = mapElement.leaflet;
+  var map = mapElement.map;
+
+  map.scrollWheelZoom.disable();
+
+  var focused = false;
+
+  var all = "Count";
+
+var onEachFeature = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+  layer.on({
+  	     mouseover: function(e) {
+        layer.setStyle({ weight: 2.5, fillOpacity: .7 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 1.5, fillOpacity: 0.4
+         });
+      }
+    });
+};
+var getColor = function(d) {
+    var value = d[all];
+    if (typeof value == "string") {
+      value = Number(value.replace(/,/, ""));
+    }
+    // console.log(value)
+    if (typeof value != "undefined") {
+      // condition ? if-true : if-false;
+     return value >= 400.00 ? '#003b21' :
+     		value >= 300.00 ? '#076e45' :
+     		value >= 200.00 ? '#4cb85e' :
+            value >= 100.00 ? '#addd8e' :
+            value >= 0 ? '#f7fcb9' :
+             
+             '#f1f2f2' ;
+    } else {
+      return "gray"
+    }
+  };
+
+  var style = function(feature) {
+    var s = {
+      fillColor: getColor(feature.properties),
+      weight: 1.5,
+      opacity: .1,
+      color: '#000',
+      fillOpacity: 0.4
+    };
+    return s;
+  }
+
+  var geojson = L.geoJson(data, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+}
+
+ map.scrollWheelZoom.disable();
